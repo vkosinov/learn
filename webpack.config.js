f01ac610c7bdcf2e663b83eab0c81bba03671a34
Viktor Kosinov <vkosinov@ya.ru>
2022-01-15 16:31:29 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mode: process.env.NODE_ENV,
+  output: {
+    filename: 'scripts.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+}
