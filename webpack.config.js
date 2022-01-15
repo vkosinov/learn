79a00afae083ce33524c6f5f7228b1f6567b7b19
Viktor Kosinov <vkosinov@ya.ru>
2022-01-15 17:04:29 +0300

    add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,27 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   mode: process.env.NODE_ENV,
+
+  entry: './src/js/index.js',
+
   output: {
     filename: 'scripts.js',
+    path: path.resolve(__dirname, 'dist'),
   },
+
+  devServer: {
+    static: './dist',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/public/index.html',
+      favicon: './src/public/favicon.ico',
+    }),
+  ],
+
   module: {
     rules: [
       {
