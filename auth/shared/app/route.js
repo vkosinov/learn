c0998b904b01e02a19be43f52bba62f5fca4af9e
Viktor Kosinov <vkosinov@ya.ru>
2024-02-04 13:49:40 +0300

    auth:session add nonce to csp

--- auth/shared/app/route.js
+++ auth/shared/app/route.js
@@ -2,31 +2,31 @@ const express = require('express')
 const router = express.Router()
 
 router.route('/').get((req, res) => {
-  res.render('pages/login')
+  res.render('pages/login', { nonce: req.nonce })
 })
 
 router.route('/user').get((req, res) => {
-  res.render('pages/user')
+  res.render('pages/user', { nonce: req.nonce })
 })
 
 router.route('/recovery').get((req, res) => {
-  res.render('pages/recovery')
+  res.render('pages/recovery', { nonce: req.nonce })
 })
 
 router.route('/reset').get((req, res) => {
-  res.render('pages/reset')
+  res.render('pages/reset', { nonce: req.nonce })
 })
 
 router.route('/users').get((req, res) => {
-  res.render('pages/users')
+  res.render('pages/users', { nonce: req.nonce })
 })
 
 router.route('/registration').get((req, res) => {
-  res.render('pages/register')
+  res.render('pages/register', { nonce: req.nonce })
 })
 
 router.route('/comments').get((req, res) => {
-  res.render('pages/comments')
+  res.render('pages/comments', { nonce: req.nonce })
 })
 
 module.exports = router
