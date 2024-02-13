78459975eb3274771c1530742f302649262dd565
Viktor Kosinov <vkosinov@ya.ru>
2024-02-13 20:21:07 +0300

    react:redux-saga: add card counter

--- react/redux-saga/src/features/cart/ui/index.tsx
+++ react/redux-saga/src/features/cart/ui/index.tsx
@@ -1,13 +1,30 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '../../../store'
+import { cartSlice } from '../slice'
+
 export const Cart = () => {
+  const count = useSelector((state: RootState) => state.cart.value)
+  const dispatch = useDispatch()
+
+  const { increment, decrement } = cartSlice.actions
+
+  const handleIncrement = () => {
+    dispatch(increment())
+  }
+
+  const handleDecrement = () => {
+    dispatch(decrement())
+  }
+
   return (
     <div>
       <h1>Card Counter</h1>
       <p>
-        <button>+</button>
-        <button>-</button>
+        <button onClick={handleIncrement}>+</button>
+        <button onClick={handleDecrement}>-</button>
       </p>
 
-      <h2>Total:</h2>
+      <h2>Total:{count}</h2>
     </div>
   )
 }
--- react/redux-saga/src/store/index.ts
+++ react/redux-saga/src/store/index.ts
@@ -2,3 +2,5 @@ import { configureStore } from '@reduxjs/toolkit'
 import { cartSlice } from '../features/cart'
 
 export const store = configureStore({ reducer: { cart: cartSlice.reducer } })
+
+export type RootState = ReturnType<typeof store.getState>
