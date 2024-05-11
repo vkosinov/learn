4542cd60af4eab73fa3a7601e5058eae3c5c7525
Viktor Kosinov <vkosinov@ya.ru>
2024-05-11 20:24:52 +0300

    add todo list

--- fp/racket/todo-list.rkt
+++ fp/racket/todo-list.rkt
@@ -2,18 +2,23 @@
 
 (require data/gvector)
 
-;
-(define (string-trim-right srt) (substring srt 0 (- (string-length srt) 1)))
-
-;Создает пустой список (расширяемый вектор)
+; Создает пустой список (расширяемый вектор)
 (define todo-list (gvector))
 
-;Добавляет новую задачу в список
-(define (add-task task)
-  (gvector-add! todo-list task))
+; Добавляет новую задачу в список
+(define (add-task)
+  (system "clear")
+  (printf "Введите задачу: ")
+  (read-line)
+  (gvector-add! todo-list (read-line)))
 
-;Удаляет задачу из списка по индексу
-(define (remove-task index)
+; Удаляет задачу из списка по индексу
+(define (remove-task)
+  (system "clear")
+  (print-tasks) ; Выводим список задач для наглядности
+  (printf "Введите индекс удаляемой задачи: ")
+  (define index (read))
+
   (if (and (integer? index)                    ; Проверяем, что index является целым числом
            (>= index 0)                        ; Проверяем, что index неотрицателен
            (< index (gvector-count todo-list))) ; Проверяем, что index меньше длины списка
@@ -22,69 +27,48 @@
         (printf "Задача под индексом ~a удалена\n" index))
       (printf "Неверный индекс задачи! \n")))
 
-;Проверем не пуст ли список задач
-(define (is-empty-todo-list function)
-  (if (= 0 (gvector-count todo-list))
-      (printf "\nСписок задач пуст!\n")
-       (function)
-  )
-)
-
-;Выводит на экран список задач
+; Выводит на экран список задач
 (define (print-tasks)
-  (begin
+  (system "clear")
+  (if (= 0 (gvector-count todo-list)) ; Проверем не пуст ли список задач
+      (printf "\nСписок задач пуст!\n")
+      (begin
         (printf "\nСписок задач:\n")
-        (for ([idx (in-range (gvector-count todo-list))])
-          (printf "~a: ~a\n" (add1 idx) (gvector-ref todo-list idx)))
-  )
-)
+        (for ([idx (in-range (gvector-count todo-list))]) ; Проходим по вектору и выводим значения
+          (printf "~a: ~a\n" idx (gvector-ref todo-list idx))))))
 
-;Выводит на экран список доступных команд
+; Выводит на экран список доступных команд
 (define (command-print)
-    (display "--------------------- \n")
-    (display "Доступные команды: \n")
-    (display "--------------------- \n")
-    (display "1. Список задач \n")
-    (display "2. Добавить задачу \n")
-    (display "3. Удалить задачу \n")
-    (display "4. Выйти\n")
-    (display "--------------------- \n")
-    (display "Введите номер команды: "))
+  (display "--------------------- \n")
+  (display "Доступные команды: \n")
+  (display "--------------------- \n")
+  (display "1. Список задач \n")
+  (display "2. Добавить задачу \n")
+  (display "3. Удалить задачу \n")
+  (display "4. Выйти\n")
+  (display "--------------------- \n")
+  (display "Введите номер команды: "))
 
 (define (main)
   (let loop ()
-    (command-print)
-
+    (command-print) ; Выводим список доступных команд
     (let ((choice (read)))
       (cond
-
-      ((= choice 1)
-        (system "clear")
-        (is-empty-todo-list print-tasks)
-        (loop))
-
-        ((= choice 2)
-          (system "clear")
-          (printf "Введите задачу: ")
-          (read-line)
-          (define input (read-line))
-          (add-task input)
+        ((= choice 1) ; Вывести список задач
+         (print-tasks)
          (loop))
-
-        ((= choice 3)
-          (system "clear")
-          (is-empty-todo-list print-tasks)
-          (printf "Введите индекс удаляемой задачи: ")
-          (remove-task (read))
+        ((= choice 2) ; Добавить задачу
+         (add-task)
          (loop))
-
-        ((= choice 4)
-          (system "clear")
-         (printf "Выход... \n"))
-
-        (else
-          (system "clear")
-         (printf "Неверная команда! \n")
+        ((= choice 3) ; Удалить задачу
+         (remove-task)
+         (loop))
+        ((= choice 4) ; Выйти
+         (system "clear")
+         (printf "Выход..."))
+        (else ; Обработка на случай если введен номер не из допусимого списка
+         (system "clear")
+         (printf "Введен недопустимый номер задачи! \n")
          (loop))))))
 
 (main)
